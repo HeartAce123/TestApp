8bfb0b3fe643a72783a5f7650ed0211da813b7b2
HeartAce123 <heartoverheat@gmail.com>
2013-12-28 20:12:19 +0700

    Initial Commit

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by LieuHaiDang on 12/28/13.
+//  Copyright (c) 2013 LieuHaiDang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
